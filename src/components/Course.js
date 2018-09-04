542a19386a6b35cd912acef0a9345376b76a74de
Unknown <dillon.embreus@gmail.com>
2018-09-04 15:20:43 -0600

    added navbar

---
+++ src/components/Course.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const Course = props => {
+    return(
+        <div>
+            { props.course ? (
+                <Card>
+                    <CardMedia style={{height: 0, paddingTop: '56.25%'}}
+                    image={props.course.fields.course.courseImage.fields.file.url}
+                    title={props.course.fields.title}/>
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            {props.course.fields.title}
+                        </Typography>
+                        <Typoghrapy component="p">
+
+                        </Typoghrapy>
+                    </CardContent>
+                </Card>
+            ): null}
+        </div>
+    )
+};
+export default Course
